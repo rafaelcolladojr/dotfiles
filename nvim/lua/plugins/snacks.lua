8554f28ececeb99b9597a159ec66bd50006a4f49
Rafael Collado <rcollado@wisdomtree.com>
2026-05-12 11:58:58 -0400

    Updated snacks dashboard layout

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -20,12 +20,14 @@ return {
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
         },
-        sections = {
-          { section = "header" },
-          { section = "keys", gap = 1, padding = 1 },
-          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2 },
-          { section = "startup" },
-        },
+        {
+          sections = {
+            { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+            { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { section = "startup" },
+          },
+        }
       },
       dim = {},
       input = {},
